7d0acfa805c0c638b3808c34925ad9ab48b296bc
fridayknight13 <73671454+madams1972@users.noreply.github.com>
2022-12-05 21:11:01 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-const sequelize = require('./config/connection');
+
+const termData = require('./terms.json');
+const PORT = 3001;
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Create the Handlebars.js engine object with custom helper functions
-const hbs = exphbs.create({ helpers });
+app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
 
-// Inform Express.js which template engine we're using
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.get('/api', (req, res) => res.json(termData));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
