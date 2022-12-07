6ce0e077f15479e7a81fdd1628db87585a25b9b1
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2022-12-06 21:03:58 -0600

    Added the front page and got the server up and running

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-// const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const routes = require('./routes/routes');
+const helpers = require('./utils/helpers/helpers')
 const sequelize = require('./config/connection');
 const path = require('path');
 
@@ -9,32 +9,37 @@ const app = express();
 const PORT = 3001;
 
 // Create the Handlebars.js engine object with custom helper functions
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 // Inform Express.js which template engine we're using
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-//app.set('views', './views');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
-app.get('/send', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/send.html'))
-);
+// app.get('/', (req, res) => {
+//   res.render('homepage');
+// });
 
-// app.use(routes);
+// app.get('/login', (req, res) => {
+//   res.render('login');
+// });
+
+// app.get('/send', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/send.html'))
+// );
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
 
-app.get('/paths', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/paths.html'))
-);
+// app.get('/paths', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/paths.html'))
+// );
 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
