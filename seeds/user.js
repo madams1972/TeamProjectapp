b38a2bd707742c382f0bea4564849ec0209bb1c8
mattmald <Mat.maldonado012@gmail.com>
2022-12-12 21:09:03 -0600

    seeeeeeeeeds

---
+++ seeds/user.js
@@ -0,0 +1,18 @@
+[
+    {
+      "username": "mattmald",
+      "password": "codinguisfun"
+    },
+    {
+        "username": "madms1972",
+        "password": "codingisfun"
+    },
+    {
+        "username": "HTML-No0b",
+        "password": "codingisfun"
+    },
+    {
+        "username": "sophiekedrowki",
+        "password": "codingisfun"
+    }
+]
