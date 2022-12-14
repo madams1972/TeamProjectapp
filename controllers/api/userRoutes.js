cb6268c84726fd2facf7ea0717e3823055e89d04
mattmald <Mat.maldonado012@gmail.com>
2022-12-13 19:55:20 -0600

    userroutes update

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
-const router = require("express").Router()
-const  User  = require("../../models/user");
+const router = require("express").Router();
+const User = require("../../models/user");
 
 // router.post('/login', async(req, res)=>{
 //     try {
@@ -8,26 +8,22 @@ const  User  = require("../../models/user");
 //         console.log(userData)
 //     if (!userData){
 //         res
-// render.post("/", async (req, res) => {
-//   try {
-//     const userData = await User.create({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: req.body.password,
-//     });
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
 
-//     req.session.save(() => {
-//       req.session.loggedIn = true;
-//       res.status(200).json(userData);
-//     });
-//   } catch {
-//     err;
-//   }
-//   {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 router.post("/login", async (req, res) => {
   try {
